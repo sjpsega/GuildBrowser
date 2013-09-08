30ec38e0995b667aa62e8dcb5bbb139d5532c017
sjpsega <ssjjpp520@126.com>
2013-09-08 18:59:19 +0800

    init

---
+++ GuildBorwser/GuildBorwser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GuildBorwser
+//
+//  Created by sjpsega on 13-9-8.
+//  Copyright (c) 2013年 sjp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
